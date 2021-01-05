48295ec208750ccfb36ef7d275a49610766c1e3a
Slava <sforce05@gmail.com>
2021-01-05 18:51:02 +0300

    more fix

---
+++ Let'sUp/Explore/EploreViewController.swift
@@ -0,0 +1,42 @@
+//
+//  EploreViewController.swift
+//  Let'sUp
+//
+//  Created by User on 05.01.2021.
+//
+
+import UIKit
+
+class EploreViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource{
+    
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath)
+    -> UICollectionReusableView {
+    let headerView =
+    collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: "header", for: indexPath)
+    return headerView
+    }
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        20
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "exploreCell", for: indexPath)
+        return cell
+    }
+    
+    
+    
+    
+}
